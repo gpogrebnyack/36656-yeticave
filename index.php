2f29d0df645e659e35aad18b7f04ff988b0f59ca
Georgy Pogrebnyak <krendel.mag@gmail.com>
2018-01-31 21:58:51 +0700

    Вернул точки с запятой

--- index.php
+++ index.php
@@ -30,10 +30,10 @@ $user_avatar = 'img/user.jpg';
         <!-- здесь PHP код для показа аватара пользователя -->
         <?php if ($is_auth == true): ?>
             <div class="user-menu__image">
-                <img src="<?=$user_avatar?>" width="40" height="40" alt="Пользователь">
+                <img src="<?=$user_avatar;?>" width="40" height="40" alt="Пользователь">
             </div>
             <div class="user-menu__logged">
-                <p><?=$user_name?></p>
+                <p><?=$user_name;?></p>
             </div>
         <?php else: ?>
             <ul class="user-menu__list">
