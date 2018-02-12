c4350dc25ede819c6e01e439628f4ec07f8d8991
Georgy Pogrebnyak <krendel.mag@gmail.com>
2018-02-12 19:22:43 +0800

    Исправил ошибку в переменной

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-require_once ('functions.php');
-
 $is_auth = (bool) rand(0, 1);
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
@@ -45,17 +43,20 @@ $lots = [
     ]
 ];
 
+require_once ('functions.php');
+
 $page_content = render('templates/index.php', [
     'lots' => $lots,
-    'time-left' => time_left()]);
+    'time_left' => time_left()
+    ]);
 $layout_content = render('templates/layout.php', [
     'title' => 'Главная',
     'is_auth' => $is_auth, 
     'user_name' => $user_name, 
     'user_avatar' => $user_avatar,
     'content' => $page_content,
-    'cat' => $cat]);
+    'cat' => $cat
+    ]);
 
 print($layout_content);
-
 ?>
